fccca8f626e180f8789ee40dcf0a17010b8a73c3
CaioVitor1 <caiovitor33@hotmail.com>
2023-01-10 13:59:05 -0300

    feat: create multiplication table

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+public class App {
+    public static void main(String[] args) {
+        final int numberToMultiply = 7;
+        System.out.println("Tabela de multiplicação do 7 :");
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println("7 * " + i + " = " + numberToMultiply * i);
+        }
+    }
+}
